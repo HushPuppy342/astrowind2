8e0a02013bb59e3ec3027c307ca8b5720369ad27
HushPuppy342 <mbarrett@conceptinc.ca>
2024-11-29 14:27:10 -0500

    Run severless

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,3 +4,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+module.exports = {
+  buildOptions: {
+    target: 'serverless',
+  },
+};
